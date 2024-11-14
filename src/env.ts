1891da447d27c21df1208bf8f87a28df3c794512
Rab Karl Colasino <colasinorabkarl@gmail.com>
2024-11-14 18:33:39 +0800

    build: remove NODE_ENV

--- src/env.ts
+++ src/env.ts
@@ -7,9 +7,6 @@ export const env = createEnv({
         GOOGLE_ID: z.string(),
         GOOGLE_SECRET: z.string(),
         NEXTAUTH_SECRET: z.string(),
-        NODE_ENV: z
-            .enum(["development", "test", "production"])
-            .default("development"),
     },
     client: {
         NEXT_PUBLIC_GOOGLE_MAP_API: z.string(),
@@ -19,7 +16,6 @@ export const env = createEnv({
         DATABASE_URL: process.env.DATABASE_URL,
         GOOGLE_ID: process.env.GOOGLE_ID,
         GOOGLE_SECRET: process.env.GOOGLE_SECRET,
-        NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         NEXT_PUBLIC_GOOGLE_MAP_API: process.env.NEXT_PUBLIC_GOOGLE_MAP_API,
         NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
